2f647c579d078b410feb2509db1bbda1cf542c72
Fedor Gorbunov <raigfp@gmail.com>
2019-09-29 00:39:51 +0500

    fix

--- src/config/settings.py
+++ src/config/settings.py
@@ -126,8 +126,8 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/videos/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'videos')
 
 AUTH_USER_MODEL = 'cuser.CUser'
 
